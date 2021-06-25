136c9f5e7f50381ae0a2390c4994c213cf8d0322
abi-200 <abitav.2000@gmail.com>
2021-06-24 18:28:06 -0700

    lab1draft

---
+++ Week1/js/init.js
@@ -0,0 +1,16 @@
+let y = 2 + 2
+const date = 23
+console.log(y)
+console.log("hello asian am 191a!");
+// JavaScript const variable declaration
+const map = L.map('map').setView([34.0709, -118.444], 15);
+
+// Leaflet tile layer, i.e. the base map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+//JavaScript let variable declaration to create a marker
+let marker = L.marker([34.0709, -118.444]).addTo(map)
+		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
+		.openPopup();
